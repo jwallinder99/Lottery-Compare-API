f41db14f0dffc1efd5fc98f94ac2d218c0c6964d
jwallinder <jjpwallinder@gmail.com>
2024-05-13 10:33:07 +0200

    Changed expected props from request body

--- api/lottery-compare.route.js
+++ api/lottery-compare.route.js
@@ -5,7 +5,11 @@ const router = Router();
 const jsonParser = bodyParser.json();
 
 router.post("/lottery-compare", jsonParser, (req, res) => {
-	res.send(`Welcome, ${req.body.username}`);
+	const { start_date, end_date, user_numbers, secondary_numbers } = req.body;
+
+	res.send(
+		`Recieved ${start_date}, ${end_date}, ${user_numbers}, ${secondary_numbers}`
+	);
 });
 
 module.exports = router;
