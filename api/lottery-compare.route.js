dc627b46b2e27e78354a6e5c2a93326c648af87c
jwallinder <jjpwallinder@gmail.com>
2024-05-13 10:35:25 +0200

    Edited response object

--- api/lottery-compare.route.js
+++ api/lottery-compare.route.js
@@ -7,9 +7,14 @@ const jsonParser = bodyParser.json();
 router.post("/lottery-compare", jsonParser, (req, res) => {
 	const { start_date, end_date, user_numbers, secondary_numbers } = req.body;
 
-	res.send(
-		`Recieved ${start_date}, ${end_date}, ${user_numbers}, ${secondary_numbers}`
-	);
+	const responseBody = {
+		start_date: start_date,
+		end_date: end_date,
+		user_numbers: user_numbers,
+		secondary_numbers: secondary_numbers,
+	};
+
+	res.json(responseBody);
 });
 
 module.exports = router;
