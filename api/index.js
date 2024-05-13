330a626a96945d455b26b9993894280fbeaf8d3a
jwallinder <jjpwallinder@gmail.com>
2024-05-13 09:44:49 +0200

    Added lottery-compare post route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+const routes = require("./lottery-compare.js");
 app.get("*", (req, res) => {
 	res.send("Welcome to Lottery-Compare API");
 });
+
+app.use("/", routes);
 
 module.exports = app;
--- api/lottery-compare.js
+++ api/lottery-compare.js
@@ -1,6 +1,11 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
-// router.post("/", (req, res) => {
-//   const { user}
-// })
+router.post("/lottery-compare", jsonParser, (req, res) => {
+	const { user_numbers } = req.body;
+	return res.send(`User Numbers: ${user_numbers}`);
+});
+
+module.exports = router;
